2fd545d1d262e5bd87e06aca7db8be084d4d0c6e
Jan Kowalski <marcinbn10game@gmail.com>
2023-07-02 13:31:36 +0200

    first commit

---
+++ src/main/java/pl/dundersztyc/fitnessapp/FitnessAppApplication.java
@@ -0,0 +1,13 @@
+package pl.dundersztyc.fitnessapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dundersztyc/fitnessapp/FitnessAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dundersztyc.fitnessapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
